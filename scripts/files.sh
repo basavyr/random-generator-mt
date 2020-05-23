ff21a0f4b14104a52c2ef865d68d16f49b2049af
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-05-23 18:12:44 +0300

    the scripts are now uploaded to repo

---
+++ scripts/files.sh
@@ -0,0 +1,2 @@
+/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++ -std=c++17 ../src/container.cc ../src/filestream.cc ../tests/file_test.cc -o files.out
+./files.out
