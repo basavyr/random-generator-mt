d45da5020c7b65ccdd4b7c1ffe4e9cacbd92dce6
Robert Poenaru <robert.poenaru@protonmail.ch>
2020-05-25 20:12:15 +0300

    adds timing benchmark. adds official ubuntu image to pipeline

--- tests/file_test.cc
+++ tests/file_test.cc
@@ -6,7 +6,7 @@ int main()
 {
     const std::string file_name = "rng_data";
     const std::string file_ext = ".rng";
-    const int n_files = 250;
+    const int n_files = 300;
     //creates the files in which containers are stored.
     auto file = std::make_unique<file::Filestream>(file_name, n_files, file_ext);
 }
